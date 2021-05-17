61be34cb315aa87b085e0ba56f038ecc08fbfe6b
Krishpandya682 <76144309+Krishpandya682@users.noreply.github.com>
2021-05-17 20:49:03 +0530

    Registration Page Added

--- SportsBuddy/settings.py
+++ SportsBuddy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration.apps.RegistrationConfig' ,
     'SportsBuddyApp.apps.SportsbuddyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
