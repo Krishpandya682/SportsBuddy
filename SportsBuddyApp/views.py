c82cd771559f15988ac6d80cccdd924b22465379
Krishpandya682 <76144309+Krishpandya682@users.noreply.github.com>
2021-05-17 09:52:31 +0530

    Basic Setup

--- SportsBuddyApp/views.py
+++ SportsBuddyApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the SportsBuddy index which is under construction")
+     return render(request, 'SportsBuddyApp/index.html', {})
