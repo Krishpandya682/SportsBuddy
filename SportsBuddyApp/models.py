a6f163b20aad8bc224f429130a90e5a5022a25c6
Aryan Marwah <aryan8m@gmail.com>
2021-05-28 09:25:48 -0500

    Event_name added to eventdetail model

--- SportsBuddyApp/models.py
+++ SportsBuddyApp/models.py
@@ -25,6 +25,7 @@ class Sport(models.Model):
 
 
 class Event(models.Model):
+    event_name=models.CharField(max_length=50,default="Event Name")
     creator = models.ForeignKey(
         User, on_delete=CASCADE, related_name='creator', default=None)
     sport = models.ForeignKey(Sport, on_delete=CASCADE)
